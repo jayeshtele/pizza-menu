cb42101b26dd8a1582553b2e83afae283b35b545
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-06-24 12:03:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -70,6 +70,7 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+      <p>Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious.</p>
 
       {pizzaLen > 0 ? (
         <ul className="pizzas">
